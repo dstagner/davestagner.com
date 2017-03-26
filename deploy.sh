129cffb61e010ddac5b96fedc26165f1b5c8abc2
Dave Stagner <dave@spnz.org>
2017-03-26 14:55:36 -0500

    moving deployment to google cloud

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
 hugo --theme=hugo-tranquilpeak-theme
-s3cmd sync --delete-removed --acl-public --reduced-redundancy --cf-invalidate public/ s3://davestagner.com/ --verbose
+#s3cmd sync --delete-removed --acl-public --reduced-redundancy --cf-invalidate public/ s3://davestagner.com/ --verbose
+
+gsutil rsync -R public gs://davestagner.com
+gsutil acl ch -r -u AllUsers:R gs://davestagner.com
